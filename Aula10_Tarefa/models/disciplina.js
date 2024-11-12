e662c3dc823cda3569851f7dcd1b58c94f5e7a93
aluno <aluno@aluno>
2024-11-12 20:27:01 -0300

    prova

--- Aula10_Tarefa/models/disciplina.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Disciplina = mongoose.model("Disciplina", {
-    nome: String,
-    cargaHoraria: Number,
-    sala: String,
-});
-
-module.exports = Disciplina;
---
+++ prova/models/exame.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Exame = mongoose.model('Exame', {
+    nome: String,
+    clinica: String,
+    id_cliente:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cliente'
+    },
+  
+});
+
+module.exports = Exame;
